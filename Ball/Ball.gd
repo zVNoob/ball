c3471425b7def4fac6f791c102cc99bb53c9599a
zVNoob <kjen.1342006@gmail.com>
2022-04-24 21:48:55 +0700

    đéo có gì ngoài mấy cái sờ kiu

---
+++ Ball/Ball.gd
@@ -0,0 +1,29 @@
+extends KinematicBody2D
+
+var MovementVector = Vector2(0,0)
+export var Speed = 150
+export var Dash = 50
+var miny =2000
+func _physics_process(delta):
+	#common movement
+	if Input.is_action_pressed("ui_left"):MovementVector.x = -Speed
+	elif Input.is_action_pressed("ui_right"):MovementVector.x = Speed
+	else:MovementVector.x = 0
+	#gravity
+	MovementVector.y = min(MovementVector.y + Speed * 2 * delta, 5000)
+	#apply vector to object
+
+func _on_FailSkill_Acitvated():
+	MovementVector.y = 5000
+
+func _on_JumpSkill_Acitvated():
+	MovementVector.y = -Speed * 1.6
+
+func _on_FlySkill_Acitvated():
+	MovementVector.y = -Speed 
+
+func _on_DashSkill_Acitvated():
+	if MovementVector.y>50:position.y = min(position.y + Dash, 567)
+	if MovementVector.y<-50:position.y = max(position.y - Dash, 14)
+	if MovementVector.x>0:position.x = min(1285, position.x + Dash)
+	if MovementVector.x<0:position.x = max(15, position.x - Dash)
---
+++ CDButton/CDButton.gd
@@ -0,0 +1,46 @@
+extends TextureProgress
+
+signal On_Skill_Acitvated
+export var Cooldown = 0.1
+export var Charge_Max = 1
+export var Restore_Per_Sec = 100
+export var Mana_Per_Use = 1000
+export var Normal_Color = Color()
+export var Charged_Color = Color()
+export var Activate_Input = ""
+export var Activate_Key = ""
+var CD = 0.0
+var Mana = 0.0
+var Usable = true
+var Activating = false
+func _ready():
+	Mana = Charge_Max * 1000
+	$ShortCut/Label.text = Activate_Key
+func _physics_process(delta):
+	#Mana regenation and effect
+	Mana = min(1000 * Charge_Max, Mana + Restore_Per_Sec * delta)
+	$Charged.visible = Charge_Max > 1
+	$Charged/Label.text = str(floor(Mana / 1000))
+	if Mana < 1000 * Charge_Max:
+		value = Mana - floor(Mana / 1000)*1000
+	else:
+		value = 1000
+	if Mana <= 1000:
+		tint_under = Color("4c4c4c")
+		if Usable: tint_progress = Normal_Color
+		else: tint_progress = Color(Normal_Color.r,Normal_Color.g,Normal_Color.b,0.5)
+	else:
+		tint_under = Normal_Color
+		tint_progress = Charged_Color
+	#Skill usage
+	if Mana < Mana_Per_Use: Usable = false
+	if Mana >= 1000: Usable = true
+	$ShortCut/CoolDown.value = (Cooldown - CD)/Cooldown*100
+	CD = max(0, CD - delta)
+	if Input.is_action_just_pressed(Activate_Input):Activating = true
+	if Input.is_action_just_released(Activate_Input):Activating = false
+	if Activating:
+		if ((CD==0) and (Mana>=Mana_Per_Use) and Usable):
+			Mana -= Mana_Per_Use
+			CD = Cooldown
+			emit_signal("On_Skill_Acitvated")
